f43a61e06ac340752fee706a6a75f7e96c535f28
Sean Owen <sowen@cloudera.com>
2014-07-31 14:09:08 +0100

    Refactor ConsumeData, avoid setting Kafka group base, set unique Kafka groups, and put in place a not-yet-working framework for reading update queue data in test

--- kafka-util/src/main/java/com/cloudera/oryx/kafka/util/ConsumeData.java
+++ kafka-util/src/main/java/com/cloudera/oryx/kafka/util/ConsumeData.java
@@ -16,10 +16,12 @@
 package com.cloudera.oryx.kafka.util;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import com.google.common.collect.AbstractIterator;
 import kafka.consumer.Consumer;
 import kafka.consumer.ConsumerConfig;
 import kafka.consumer.KafkaStream;
@@ -30,71 +32,86 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * A process that will consume data from a Kafka queue, optionally in parallel, and log
+ * A iterator that consumes data from a Kafka queue. When run on the command line, logs
  * the results to the console.
  */
-public final class ConsumeData {
+public final class ConsumeData implements Iterable<String[]> {
 
   private static final Logger log = LoggerFactory.getLogger(ConsumeData.class);
 
-  public static final String DEFAULT_GROUP = "OryxGroup";
-
-  private final int zkPort;
   private final String topic;
-  private final String groupID;
+  private final int zkPort;
 
   public ConsumeData() {
-    this(DEFAULT_GROUP);
+    this(ProduceData.DEFAULT_TOPIC);
   }
 
-  public ConsumeData(String groupID) {
-    this(LocalKafkaBroker.DEFAULT_ZK_PORT, ProduceData.DEFAULT_TOPIC, groupID);
+  public ConsumeData(String topic) {
+    this(topic, LocalKafkaBroker.DEFAULT_ZK_PORT);
   }
 
-  public ConsumeData(int zkPort, String topic, String groupID) {
-    this.zkPort = zkPort;
+  public ConsumeData(String topic, int zkPort) {
     this.topic = topic;
-    this.groupID = groupID;
+    this.zkPort = zkPort;
   }
 
   public static void main(String[] args) {
     int numThreads = args.length < 1 ? 1 : Integer.parseInt(args[0]);
     if (numThreads > 1) {
       for (int i = 0; i < numThreads; i++) {
-        final String group = DEFAULT_GROUP + i;
         new Thread(new Runnable() {
           @Override
           public void run() {
-            new ConsumeData(group).start();
+            for (String[] km : new ConsumeData()) {
+              log.info("{} = {}", km[0], km[1]);
+            }
           }
         }).start();
       }
     } else {
-      new ConsumeData(DEFAULT_GROUP).start();
+      for (String[] km : new ConsumeData()) {
+        log.info("{} = {}", km[0], km[1]);
+      }
     }
   }
 
-  public void start() {
+  @Override
+  public Iterator<String[]> iterator() {
     Properties consumerProps = new Properties();
-    consumerProps.setProperty("group.id", groupID);
+    consumerProps.setProperty("group.id", "OryxGroup-ConsumeData");
     consumerProps.setProperty("zookeeper.connect", "localhost:" + zkPort);
-
     ConsumerConfig consumerConfig = new ConsumerConfig(consumerProps);
     ConsumerConnector consumer = Consumer.createJavaConsumerConnector(consumerConfig);
+    return new ConsumeDataIterator(topic, consumer);
+  }
 
-    Map<String,Integer> topicCountMap = new HashMap<>();
-    topicCountMap.put(topic, 1);
+  private static final class ConsumeDataIterator extends AbstractIterator<String[]> {
 
-    Map<String,List<KafkaStream<String,String>>> consumerMap =
-        consumer.createMessageStreams(topicCountMap, new StringDecoder(null), new StringDecoder(null));
-    List<KafkaStream<String,String>> streams = consumerMap.get(topic);
+    private final ConsumerConnector consumer;
+    private final Iterator<MessageAndMetadata<String,String>> iterator;
 
-    try {
-      for (MessageAndMetadata<String, String> msg : streams.get(0)) {
-        log.info("{} = {}", msg.key(), msg.message());
+    private ConsumeDataIterator(String topic, ConsumerConnector consumer) {
+      this.consumer = consumer;
+      Map<String,Integer> topicCountMap = new HashMap<>();
+      topicCountMap.put(topic, 1);
+
+      Map<String,List<KafkaStream<String,String>>> consumerMap =
+          consumer.createMessageStreams(topicCountMap,
+                                        new StringDecoder(null),
+                                        new StringDecoder(null));
+      KafkaStream<String,String> stream = consumerMap.get(topic).get(0);
+      this.iterator = stream.iterator();
+    }
+
+    @Override
+    protected String[] computeNext() {
+      if (iterator.hasNext()) {
+        MessageAndMetadata<String,String> mm = iterator.next();
+        return new String[] { mm.key(), mm.message() };
+      } else {
+        consumer.shutdown();
+        return endOfData();
       }
-    } finally {
-      consumer.shutdown();
     }
   }
 
