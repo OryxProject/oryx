174b8f51963b8ba579d8f39c43a98f411d6493ba
Sean Owen <sowen@cloudera.com>
2014-08-02 10:53:42 +0100

    Add actual test of update queue output for ALS

--- oryx-ml-mllib/src/test/java/com/cloudera/oryx/ml/mllib/als/ALSUpdateIT.java
+++ oryx-ml-mllib/src/test/java/com/cloudera/oryx/ml/mllib/als/ALSUpdateIT.java
@@ -18,6 +18,7 @@ package com.cloudera.oryx.ml.mllib.als;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -26,6 +27,8 @@ import java.util.List;
 import java.util.Map;
 
 import com.typesafe.config.Config;
+import org.dmg.pmml.Extension;
+import org.dmg.pmml.PMML;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,6 +36,7 @@ import org.slf4j.LoggerFactory;
 import com.cloudera.oryx.common.io.IOUtils;
 import com.cloudera.oryx.common.settings.ConfigUtils;
 import com.cloudera.oryx.ml.MLUpdate;
+import com.cloudera.oryx.ml.pmml.PMMLUtils;
 
 public final class ALSUpdateIT extends AbstractALSIT {
 
@@ -43,6 +47,7 @@ public final class ALSUpdateIT extends AbstractALSIT {
   private static final int GEN_INTERVAL_SEC = 10;
   private static final int BLOCK_INTERVAL_SEC = 1;
   private static final int FEATURES = 4;
+  private static final double LAMBDA = 0.001;
 
   @Test
   public void testALS() throws Exception {
@@ -61,36 +66,93 @@ public final class ALSUpdateIT extends AbstractALSIT {
     overlayConfig.put("batch.block-interval-sec",
                       Integer.toString(BLOCK_INTERVAL_SEC));
     overlayConfig.put("als.hyperparams.implicit", "false");
+    overlayConfig.put("als.hyperparams.lambda", Double.toString(LAMBDA));
     overlayConfig.put("als.hyperparams.features", Integer.toString(FEATURES));
     Config config = ConfigUtils.overlayOn(overlayConfig, getConfig());
 
     startMessageQueue();
 
-    startServerProduceConsumeQueues(config,
-                                    new RandomALSDataGenerator(1000, 1000, 1, 5),
-                                    DATA_TO_WRITE,
-                                    WRITE_INTERVAL_MSEC);
+    List<String[]> updates =
+        startServerProduceConsumeQueues(config,
+                                        new RandomALSDataGenerator(1000, 1000, 1, 5),
+                                        DATA_TO_WRITE,
+                                        WRITE_INTERVAL_MSEC);
 
     List<Path> modelInstanceDirs = IOUtils.listFiles(modelDir, "*");
     log.info("Model instance dirs: {}", modelInstanceDirs);
 
-    checkIntervals(modelInstanceDirs.size(), DATA_TO_WRITE, WRITE_INTERVAL_MSEC, GEN_INTERVAL_SEC);
+    int generations = modelInstanceDirs.size();
+    checkIntervals(generations, DATA_TO_WRITE, WRITE_INTERVAL_MSEC, GEN_INTERVAL_SEC);
 
-    Collection<Integer> userIDs = Collections.emptySet();
-    Collection<Integer> productIDs = Collections.emptySet();
+    List<Collection<Integer>> userIDs = new ArrayList<>();
+    userIDs.add(Collections.<Integer>emptySet());
+    List<Collection<Integer>> productIDs = new ArrayList<>();
+    productIDs.add(Collections.<Integer>emptySet());
 
     for (Path modelInstanceDir : modelInstanceDirs) {
       log.info("Testing model instance dir {}", modelInstanceDir);
       Path modelFile = modelInstanceDir.resolve(MLUpdate.MODEL_FILE_NAME);
-      assertTrue("No such model file: " + modelFile, Files.exists(modelFile));
-      assertTrue("Empty model file: " + modelFile, Files.size(modelFile) > 0);
+      assertTrue("Model file should exist: " + modelFile, Files.exists(modelFile));
+      assertTrue("Model file should not be empty: " + modelFile, Files.size(modelFile) > 0);
       Path xDir = modelInstanceDir.resolve("X");
       assertTrue(Files.exists(xDir));
-      userIDs = checkFeatures(xDir, userIDs);
+      userIDs.add(checkFeatures(xDir, userIDs.get(userIDs.size() - 1)));
       Path yDir = modelInstanceDir.resolve("Y");
       assertTrue(Files.exists(yDir));
-      productIDs = checkFeatures(yDir, productIDs);
+      productIDs.add(checkFeatures(yDir, productIDs.get(productIDs.size() - 1)));
     }
+    userIDs.remove(0);
+    productIDs.remove(0);
+
+    Collection<Integer> expectedUsers = null;
+    Collection<Integer> expectedProducts = null;
+    Collection<Integer> seenUsers = null;
+    Collection<Integer> seenProducts = null;
+    int whichGeneration = -1;
+    for (String[] km : updates) {
+
+      String type = km[0];
+      String value = km[1];
+
+      log.info("{} = {}", type, value);
+
+      boolean isModel = "MODEL".equals(type);
+      boolean isUpdate = "UP".equals(type);
+      assertTrue(isModel || isUpdate);
+
+      if (isUpdate) {
+        String[] tokens = value.split("\t");
+        boolean isUser = "X".equals(tokens[0]);
+        boolean isProduct = "Y".equals(tokens[0]);
+        Integer id = Integer.valueOf(tokens[1]);
+        assertTrue(isUser || isProduct);
+        if (isUser) {
+          seenUsers.add(id);
+        } else {
+          seenProducts.add(id);
+        }
+      } else {
+        PMML pmml = PMMLUtils.fromString(value);
+        List<Extension> extensions = pmml.getExtensions();
+        assertEquals(5, extensions.size());
+        Map<String,String> extensionKeyValues = new HashMap<>();
+        for (Extension extension : extensions) {
+          extensionKeyValues.put(extension.getName(), extension.getValue());
+        }
+        assertEquals(Integer.toString(FEATURES), extensionKeyValues.get("features"));
+        assertEquals(Double.toString(LAMBDA), extensionKeyValues.get("lambda"));
+        assertEquals("false", extensionKeyValues.get("implicit"));
+
+        assertEquals(expectedUsers, seenUsers);
+        assertEquals(expectedProducts, seenProducts);
+        whichGeneration++;
+        expectedUsers = userIDs.get(whichGeneration);
+        expectedProducts = productIDs.get(whichGeneration);
+        seenUsers = new HashSet<>();
+        seenProducts = new HashSet<>();
+      }
+    }
+
   }
 
   private static Collection<Integer> checkFeatures(Path path, Collection<Integer> previousIDs)
