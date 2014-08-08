5e7e1a632e89996a18aa37a240b89cdc752d2d89
Sean Owen <srowen@gmail.com>
2014-08-08 17:52:56 +0100

    Additional changes to move Jersey to a runtime dep

--- oryx-lambda-serving/src/main/java/com/cloudera/oryx/lambda/serving/ServingLayer.java
+++ oryx-lambda-serving/src/main/java/com/cloudera/oryx/lambda/serving/ServingLayer.java
@@ -41,7 +41,6 @@ import org.apache.tomcat.util.descriptor.web.ErrorPage;
 import org.apache.tomcat.util.descriptor.web.LoginConfig;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -227,9 +226,9 @@ public final class ServingLayer implements Closeable {
 
     context.setWebappVersion("3.1");
     context.setName("Oryx");
-    ServletContainer servletContainer = new ServletContainer(new OryxApplication(config));
-    Tomcat.addServlet(context, "jersey-container-servlet", servletContainer);
-    context.addServletMapping("/*", "jersey-container-servlet");
+
+    String jaxRSApplicationClass = config.getString("serving.application-class");
+    Tomcat.addServlet(context, "serving-layer", jaxRSApplicationClass);
 
     addErrorPages(context);
 
