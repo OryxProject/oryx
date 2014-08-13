9ff591401bea1794d8956f741be0f236319afbf8
Sean Owen <sowen@cloudera.com>
2014-08-13 16:00:39 +0100

    Add skeleton example module to show use of oryx-api

---
+++ example/src/main/java/com/cloudera/oryx/example/ExampleSpeedModelManager.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2014, Cloudera, Inc. All Rights Reserved.
+ *
+ * Cloudera, Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"). You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the
+ * License.
+ */
+
+package com.cloudera.oryx.example;
+
+import com.cloudera.oryx.lambda.KeyMessage;
+import com.cloudera.oryx.lambda.speed.SpeedModelManager;
+import org.apache.spark.api.java.JavaPairRDD;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+
+public final class ExampleSpeedModelManager implements SpeedModelManager<String,String,String> {
+
+  @Override
+  public void consume(Iterator<KeyMessage<String,String>> updateIterator) throws IOException {
+
+  }
+
+  @Override
+  public void close() {
+
+  }
+
+  @Override
+  public Collection<String> buildUpdates(JavaPairRDD<String,String> newData) {
+    return null;
+  }
+
+}
