b5832ded1b239864bb8502cc25ffe711f4c34643
Sean Owen <sowen@cloudera.com>
2014-09-03 19:43:53 +0100

    Add PairComparators

---
+++ oryx-common/src/main/java/com/cloudera/oryx/common/collection/PairComparators.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2014, Cloudera, Inc. and Intel Corp. All Rights Reserved.
+ *
+ * Cloudera, Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"). You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the
+ * License.
+ */
+
+package com.cloudera.oryx.common.collection;
+
+import java.util.Comparator;
+
+public final class PairComparators {
+
+  private PairComparators() {}
+
+  public static <K extends Comparable<K>,V> Comparator<Pair<K,V>> byFirst() {
+    return new Comparator<Pair<K,V>>() {
+      @Override
+      public int compare(Pair<K,V> p1, Pair<K,V> p2) {
+        return p1.getFirst().compareTo(p2.getFirst());
+      }
+    };
+  }
+
+  public static <K,V extends Comparable<V>> Comparator<Pair<K,V>> bySecond() {
+    return new Comparator<Pair<K,V>>() {
+      @Override
+      public int compare(Pair<K,V> p1, Pair<K,V> p2) {
+        return p1.getSecond().compareTo(p2.getSecond());
+      }
+    };
+  }
+
+}
