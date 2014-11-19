7cbe0596181ba8425e8b10f1f8f35ba6d459eb8f
Sean Owen <sowen@cloudera.com>
2014-11-19 12:49:07 +0100

    Add basic package-info files

---
+++ oryx-common/src/main/java/com/cloudera/oryx/common/package-info.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2014, Cloudera, Inc. All Rights Reserved.
+ *
+ * Cloudera, Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"). You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the
+ * License.
+ */
+
+/**
+ * Contains utility code common to all implementation modules. This should not be
+ * required by user applications, or the API or utility modules, but is common
+ * to all other modules.
+ */
+package com.cloudera.oryx.common;
