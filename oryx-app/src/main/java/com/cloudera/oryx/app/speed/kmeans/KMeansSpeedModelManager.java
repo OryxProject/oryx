1012a02373bc31d90123ff8c3cb03cb36a66113f
Suneel Marthi <suneel.marthi@gmail.com>
2015-01-23 11:06:24 -0500

    Skeletal code for KMeans Speed layer

---
+++ oryx-app/src/main/java/com/cloudera/oryx/app/speed/kmeans/KMeansSpeedModelManager.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2014, Cloudera, Inc. All Rights Reserved.
+ *
+ * Cloudera, Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"). You may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the
+ * License.
+ */
+
+package com.cloudera.oryx.app.speed.kmeans;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Iterator;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.cloudera.oryx.lambda.KeyMessage;
+import com.cloudera.oryx.lambda.speed.SpeedModelManager;
+
+public final class KMeansSpeedModelManager implements SpeedModelManager<String, String, String> {
+
+  private static final Logger log = LoggerFactory.getLogger(KMeansSpeedModelManager.class);
+
+  private KMeansSpeedModel model;
+
+  @Override
+  public void consume(Iterator<KeyMessage<String, String>> updateIterator) throws IOException {
+    while (updateIterator.hasNext()) {
+      KeyMessage<String, String> km = updateIterator.next();
+      String key = km.getKey();
+      String message = km.getMessage();
+      switch (key) {
+        case "UP":
+          break;
+        case "MODEL":
+          break;
+      }
+    }
+
+  }
+
+  @Override
+  public Iterable<String> buildUpdates(JavaPairRDD<String, String> newData) throws IOException {
+    if (model == null) {
+      return Collections.emptyList();
+    }
+
+    return null;
+  }
+
+  @Override
+  public void close() {
+    // do nothing
+  }
+}
