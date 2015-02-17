4bc46bf3a1dea423ddd5808ef3c3b44d74c51599
Sean Owen <sowen@cloudera.com>
2015-02-17 18:34:14 +0000

    Optimize some k-means eval functions

--- oryx-app-mllib/src/main/java/com/cloudera/oryx/app/mllib/kmeans/KMeansEvaluation.java
+++ oryx-app-mllib/src/main/java/com/cloudera/oryx/app/mllib/kmeans/KMeansEvaluation.java
@@ -16,12 +16,11 @@
 package com.cloudera.oryx.app.mllib.kmeans;
 
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function2;
@@ -64,22 +63,13 @@ final class KMeansEvaluation implements Serializable {
     double totalDBIndex = 0.0;
     for (int i = 0; i < numClusters; i++) {
       double maxDBIndex = 0.0;
-
-      ClusterInfo c1 = clusters.get(i);
-      double clusterScatter1 =
-          clusterSumDistAndCounts.get(i)._1() / clusterSumDistAndCounts.get(i)._2();
-      for (int j = 0; j < numClusters; j++) {
-        if (i != j) {
-          ClusterInfo c2 = clusters.get(j);
-
-          double clusterScatter2 =
-              clusterSumDistAndCounts.get(j)._1() / clusterSumDistAndCounts.get(j)._2();
-          double dbIndex = (clusterScatter1 + clusterScatter2) /
-              distanceFn.distance(c1.getCenter(), c2.getCenter());
-
-          if (dbIndex > maxDBIndex) {
-            maxDBIndex = dbIndex;
-          }
+      double[] center = clusters.get(i).getCenter();
+      double clusterScatter1 = scatter(clusterSumDistAndCounts.get(i));
+      for (int j = i + 1; j < numClusters; j++) {
+        double dbIndex = (clusterScatter1 + scatter(clusterSumDistAndCounts.get(j))) /
+            distanceFn.distance(center, clusters.get(j).getCenter());
+        if (dbIndex > maxDBIndex) {
+          maxDBIndex = dbIndex;
         }
       }
       totalDBIndex += maxDBIndex;
@@ -88,6 +78,10 @@ final class KMeansEvaluation implements Serializable {
     double daviesBouldinIndex = totalDBIndex / numClusters;
     log.info("Computed Davies-Bouldin Index for {} clusters: {}", numClusters, daviesBouldinIndex);
     return daviesBouldinIndex;
+  }
+
+  private static double scatter(Tuple2<Double,Long> sumDistAndCount) {
+    return sumDistAndCount._1() / sumDistAndCount._2();
   }
 
   /**
@@ -114,12 +108,10 @@ final class KMeansEvaluation implements Serializable {
     for (int i = 0; i < numClusters; i++) {
       double[] center = clusters.get(i).getCenter();
       // Distances are symmetric, hence d(i,j) == d(j,i)
-      for (int j = 0; j < numClusters; j++) {
-        if (i != j) {
-          double distance = distanceFn.distance(center, clusters.get(j).getCenter());
-          if (minInterClusterDistance > distance) {
-            minInterClusterDistance = distance;
-          }
+      for (int j = i + 1; j < numClusters; j++) {
+        double distance = distanceFn.distance(center, clusters.get(j).getCenter());
+        if (distance < minInterClusterDistance) {
+          minInterClusterDistance = distance;
         }
       }
     }
@@ -147,28 +139,28 @@ final class KMeansEvaluation implements Serializable {
     long sampleCount = 0L;
 
     for (Map.Entry<Integer, Iterable<double[]>> entry : clusteredPointsMap.entrySet()) {
-      Iterable<double[]> clusteredPoints = entry.getValue();
-      double silhouetteCoefficientForCluster = 0.0;
+      List<double[]> clusteredPoints = Lists.newArrayList(entry.getValue());
       // No. of points in this cluster
-      long clusterSize = Iterables.size(clusteredPoints);
+      long clusterSize = clusteredPoints.size();
       // Increment the total sample count for computing silhouette coefficient
       sampleCount += clusterSize;
       // if there's only one element in a cluster, then assume the silhouetteCoefficient for
       // the cluster = 0, this is an arbitrary choice per Section 2: Construction of Silhouettes
       // in the referenced paper
       if (clusterSize > 1) {
+        double silhouetteCoefficientForCluster = 0.0;
         for (double[] point : clusteredPoints) {
           double pointIntraClusterDissimilarity =
               clusterDissimilarityForPoint(point, clusteredPoints, true);
           double pointInterClusterDissimilarity =
               minInterClusterDissimilarityForPoint(entry.getKey(), point, clusteredPointsMap);
 
-          silhouetteCoefficientForCluster +=
-              calcSilhouetteCoefficient(pointIntraClusterDissimilarity, pointInterClusterDissimilarity);
+          silhouetteCoefficientForCluster += calcSilhouetteCoefficient(
+              pointIntraClusterDissimilarity, pointInterClusterDissimilarity);
         }
+        overallSilhouetteCoefficientForClustering += silhouetteCoefficientForCluster;
       }
 
-      overallSilhouetteCoefficientForClustering += silhouetteCoefficientForCluster;
     }
 
     double silhouetteCoefficient = overallSilhouetteCoefficientForClustering / sampleCount;
@@ -176,7 +168,7 @@ final class KMeansEvaluation implements Serializable {
     return silhouetteCoefficient;
   }
 
-  private JavaRDD<Vector> fetchSampleData(JavaRDD<Vector> evalData) {
+  private static JavaRDD<Vector> fetchSampleData(JavaRDD<Vector> evalData) {
     JavaRDD<Vector> data = evalData;
 
     if (evalData.count() > MAX_SAMPLE_SIZE) {
@@ -242,35 +234,37 @@ final class KMeansEvaluation implements Serializable {
     }).groupByKey();
   }
 
-  private double clusterDissimilarityForPoint(double[] vector, Iterable<double[]> points,
+  private double clusterDissimilarityForPoint(double[] vector,
+                                              List<double[]> points,
                                               boolean ownCluster) {
-    double dissimilarity = 0.0;
+    double totalDissimilarity = 0.0;
     for (double[] point : points) {
-      if (!Arrays.equals(vector, point)) {
-        dissimilarity += distanceFn.distance(vector, point);
-      }
+      totalDissimilarity += distanceFn.distance(vector, point);
     }
 
     if (ownCluster) {
       // (points.size -1) because a point's dissimilarity is being measured with other
       // points in its own cluster, hence there would be (n - 1) dissimilarities computed
-      return dissimilarity / (Iterables.size(points) - 1);
+      return totalDissimilarity / (points.size() - 1);
     } else {
       // point dissimilarity is being measured with all points of one of other clusters
-      return dissimilarity / Iterables.size(points);
+      return totalDissimilarity / points.size();
     }
   }
 
-  private double minInterClusterDissimilarityForPoint(int parentClusterId, double[] vector,
-                                                      Map<Integer, Iterable<double[]>> clusteredPointsMap) {
+  private double minInterClusterDissimilarityForPoint(
+      int parentClusterId,
+      double[] vector,
+      Map<Integer, Iterable<double[]>> clusteredPointsMap) {
 
     double minInterClusterDissimilarity = Double.POSITIVE_INFINITY;
 
     for (Map.Entry<Integer, Iterable<double[]>> entry : clusteredPointsMap.entrySet()) {
+      List<double[]> clusteredPoints = Lists.newArrayList(entry.getValue());
       // only compute dissimilarities with other clusters
       if (!entry.getKey().equals(parentClusterId)) {
         double otherClusterDissimilarity =
-            clusterDissimilarityForPoint(vector, entry.getValue(), false);
+            clusterDissimilarityForPoint(vector, clusteredPoints, false);
         if (otherClusterDissimilarity < minInterClusterDissimilarity) {
           minInterClusterDissimilarity = otherClusterDissimilarity;
         }
@@ -279,10 +273,11 @@ final class KMeansEvaluation implements Serializable {
     return minInterClusterDissimilarity;
   }
 
-  private double calcSilhouetteCoefficient(double d1, double d2) {
+  private static double calcSilhouetteCoefficient(double d1, double d2) {
     if (d1 < d2) {
       return 1.0 - (d1 / d2);
-    } else if (d1 > d2) {
+    }
+    if (d1 > d2) {
       return (d2 / d1) - 1.0;
     }
     return 0.0;
