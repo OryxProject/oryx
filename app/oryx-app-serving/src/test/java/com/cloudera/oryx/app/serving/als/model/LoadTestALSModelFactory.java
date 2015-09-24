7c03b629ad5bfaac58d1114899c3d6e7c2227fef
Sean Owen <sowen@cloudera.com>
2015-09-24 16:58:18 +0100

    Improve default JVM config and multi-thread creation of load benchmark for ALS

--- app/oryx-app-serving/src/test/java/com/cloudera/oryx/app/serving/als/model/LoadTestALSModelFactory.java
+++ app/oryx-app-serving/src/test/java/com/cloudera/oryx/app/serving/als/model/LoadTestALSModelFactory.java
@@ -17,6 +17,12 @@ package com.cloudera.oryx.app.serving.als.model;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.commons.math3.distribution.PoissonDistribution;
 import org.apache.commons.math3.random.RandomGenerator;
@@ -25,6 +31,7 @@ import org.slf4j.LoggerFactory;
 
 import com.cloudera.oryx.app.serving.als.TestALSRescorerProvider;
 import com.cloudera.oryx.common.lang.JVMUtils;
+import com.cloudera.oryx.common.lang.LoggingVoidCallable;
 import com.cloudera.oryx.common.math.VectorMath;
 import com.cloudera.oryx.common.random.RandomManager;
 
@@ -39,7 +46,7 @@ public final class LoadTestALSModelFactory {
   public static final int WORKERS =
       Integer.parseInt(System.getProperty("oryx.test.als.benchmark.workers", "4"));
   public static final int REQS_PER_WORKER =
-      Integer.parseInt(System.getProperty("oryx.test.als.benchmark.reqsPerWorker", "100"));
+      Integer.parseInt(System.getProperty("oryx.test.als.benchmark.reqsPerWorker", "1000"));
   private static final int FEATURES =
       Integer.parseInt(System.getProperty("oryx.test.als.benchmark.features", "100"));
   private static final int AVG_ITEMS_PER_USER =
@@ -56,31 +63,62 @@ public final class LoadTestALSModelFactory {
     System.gc();
     long startMemory = JVMUtils.getUsedMemory();
 
-    RandomGenerator random = RandomManager.getRandom();
-    PoissonDistribution itemPerUserDist = new PoissonDistribution(
-        random,
-        AVG_ITEMS_PER_USER,
-        PoissonDistribution.DEFAULT_EPSILON,
-        PoissonDistribution.DEFAULT_MAX_ITERATIONS);
-    ALSServingModel model = new ALSServingModel(FEATURES, true, LSH_SAMPLE_RATE, new TestALSRescorerProvider());
+    final ALSServingModel model = new ALSServingModel(FEATURES, true, LSH_SAMPLE_RATE, new TestALSRescorerProvider());
+    final AtomicLong totalEntries = new AtomicLong();
 
-    log.info("Adding {} users", USERS);
-    long totalEntries = 0;
-    for (int user = 0; user < USERS; user++) {
-      String userID = "U" + user;
-      model.setUserVector(userID, VectorMath.randomVectorF(FEATURES, random));
-      int itemsPerUser = itemPerUserDist.sample();
-      totalEntries += itemsPerUser;
-      Collection<String> knownIDs = new ArrayList<>(itemsPerUser);
-      for (int i = 0; i < itemsPerUser; i++) {
-        knownIDs.add("I" + random.nextInt(ITEMS));
+    int numCores = Runtime.getRuntime().availableProcessors();
+    ExecutorService executor = Executors.newFixedThreadPool(numCores);
+    try {
+
+      log.info("Adding {} users", USERS);
+      final AtomicInteger userCount = new AtomicInteger();
+      List<Callable<Void>> addUserCallables = new ArrayList<>();
+      for (int i = 0; i < numCores; i++) {
+        addUserCallables.add(new LoggingVoidCallable() {
+          private final RandomGenerator random = RandomManager.getRandom();
+          private final PoissonDistribution itemPerUserDist = new PoissonDistribution(
+              random,
+              AVG_ITEMS_PER_USER,
+              PoissonDistribution.DEFAULT_EPSILON,
+              PoissonDistribution.DEFAULT_MAX_ITERATIONS);
+          @Override
+          public void doCall() {
+            for (int user = userCount.getAndIncrement(); user < USERS; user = userCount.getAndIncrement()) {
+              String userID = "U" + user;
+              model.setUserVector(userID, VectorMath.randomVectorF(FEATURES, random));
+              int itemsPerUser = itemPerUserDist.sample();
+              totalEntries.addAndGet(itemsPerUser);
+              Collection<String> knownIDs = new ArrayList<>(itemsPerUser);
+              for (int i = 0; i < itemsPerUser; i++) {
+                knownIDs.add("I" + random.nextInt(ITEMS));
+              }
+              model.addKnownItems(userID, knownIDs);
+            }
+          }
+        });
       }
-      model.addKnownItems(userID, knownIDs);
-    }
+      executor.invokeAll(addUserCallables);
 
-    log.info("Adding {} items", ITEMS);
-    for (int item = 0; item < ITEMS; item++) {
-      model.setItemVector("I" + item, VectorMath.randomVectorF(FEATURES, random));
+      log.info("Adding {} items", ITEMS);
+      final AtomicInteger itemCount = new AtomicInteger();
+      List<Callable<Void>> addItemCallables = new ArrayList<>();
+      for (int i = 0; i < numCores; i++) {
+        addItemCallables.add(new LoggingVoidCallable() {
+          private final RandomGenerator random = RandomManager.getRandom();
+          @Override
+          public void doCall() {
+            for (int item = itemCount.getAndIncrement(); item < ITEMS; item = itemCount.getAndIncrement()) {
+              model.setItemVector("I" + item, VectorMath.randomVectorF(FEATURES, random));
+            }
+          }
+        });
+      }
+      executor.invokeAll(addItemCallables);
+
+    } catch (InterruptedException ie) {
+      throw new IllegalStateException(ie);
+    } finally {
+      executor.shutdown();
     }
 
     System.gc();
