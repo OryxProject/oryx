37cdbe1a6d5f15408f0da981c46863f8009d930a
Sean Owen <sowen@cloudera.com>
2015-11-09 16:02:45 +0100

    Closes issue #258 : make script less sensitive to JAR versions by picking last of several possibilities

--- deploy/bin/compute-classpath.sh
+++ deploy/bin/compute-classpath.sh
@@ -18,30 +18,35 @@
 HADOOP_CONF_DIR="/etc/hadoop/conf"
 CDH_JARS_DIR="/opt/cloudera/parcels/CDH/jars"
 
+function printLatest() {
+  ls -1 ${CDH_JARS_DIR}/$1 | grep -E "[0-9]\\.jar$" | tail -1
+}
+
 # Some are known to have multiple versions, and so are fully specified:
 
 echo ${HADOOP_CONF_DIR}
-ls -1 \
- ${CDH_JARS_DIR}/zookeeper-*.jar \
- ${CDH_JARS_DIR}/spark-assembly-*.jar \
- ${CDH_JARS_DIR}/spark-examples-*.jar \
- ${CDH_JARS_DIR}/hadoop-auth-*.jar \
- ${CDH_JARS_DIR}/hadoop-common-*.jar \
- ${CDH_JARS_DIR}/hadoop-hdfs-*.jar \
- ${CDH_JARS_DIR}/hadoop-mapreduce-client-core-*.jar \
- ${CDH_JARS_DIR}/hadoop-yarn-api-*.jar \
- ${CDH_JARS_DIR}/hadoop-yarn-client-*.jar \
- ${CDH_JARS_DIR}/hadoop-yarn-common-*.jar \
- ${CDH_JARS_DIR}/hadoop-yarn-server-web-proxy-*.jar \
- ${CDH_JARS_DIR}/hadoop-yarn-applications-distributedshell-*.jar \
- ${CDH_JARS_DIR}/htrace-core-3.0.4.jar \
- ${CDH_JARS_DIR}/commons-cli-1.2.jar \
- ${CDH_JARS_DIR}/commons-collections-*.jar \
- ${CDH_JARS_DIR}/commons-configuration-1.7.jar \
- ${CDH_JARS_DIR}/commons-lang-2.6.jar \
- ${CDH_JARS_DIR}/http*-4.2.5.jar \
- ${CDH_JARS_DIR}/jackson-*-1.9.12.jar \
- ${CDH_JARS_DIR}/protobuf-java-*.jar \
- ${CDH_JARS_DIR}/snappy-java-1.0.5.jar \
- ${CDH_JARS_DIR}/avro-1.7.6-*.jar \
- | grep -E "[0-9]\\.jar$"
+printLatest "zookeeper-*.jar"
+printLatest "spark-assembly-*.jar"
+printLatest "spark-examples-*.jar"
+printLatest "hadoop-auth-*.jar"
+printLatest "hadoop-common-*.jar"
+printLatest "hadoop-hdfs-2*.jar"
+printLatest "hadoop-mapreduce-client-core-*.jar"
+printLatest "hadoop-yarn-api-*.jar"
+printLatest "hadoop-yarn-client-*.jar"
+printLatest "hadoop-yarn-common-*.jar"
+printLatest "hadoop-yarn-server-web-proxy-*.jar"
+printLatest "hadoop-yarn-applications-distributedshell-*.jar"
+printLatest "htrace-core-3.*.jar"
+printLatest "commons-cli-1.*.jar"
+printLatest "commons-collections-*.jar"
+printLatest "commons-configuration-1.*.jar"
+printLatest "commons-lang-2.*.jar"
+printLatest "httpclient-4.2.*.jar"
+printLatest "httpcore-4.2.*.jar"
+printLatest "httpmime-4.2.*.jar"
+printLatest "jackson-core-asl-1.9.12.jar"
+printLatest "jackson-mapper-asl-1.9.12.jar"
+printLatest "protobuf-java-*.jar"
+printLatest "snappy-java-*.jar"
+printLatest "avro-1.7.*.jar"
