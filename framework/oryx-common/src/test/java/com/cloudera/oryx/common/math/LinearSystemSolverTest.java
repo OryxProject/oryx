f49be48aa9bf270be45ad9ba67f67b80298a207a
Sean Owen <sowen@cloudera.com>
2016-09-02 14:57:35 +0100

    Small optimization to solving for new vectors

--- framework/oryx-common/src/test/java/com/cloudera/oryx/common/math/LinearSystemSolverTest.java
+++ framework/oryx-common/src/test/java/com/cloudera/oryx/common/math/LinearSystemSolverTest.java
@@ -40,6 +40,20 @@ public final class LinearSystemSolverTest extends OryxTest {
   }
 
   @Test
+  public void testSolveDToD() {
+    double[][] a = {
+        {1.3, -2.0, 3.0},
+        {2.0, 0.0, 5.0},
+        {0.0, -1.5, 5.5},
+    };
+    Solver solver = LinearSystemSolver.getSolver(a);
+    assertNotNull(solver);
+    double[] y = solver.solveDToD(new double[] {1.0, 2.0, 6.5});
+    assertArrayEquals(
+        new double[] {-1.9560439560439564, 0.002197802197802894, 1.1824175824175824}, y);
+  }
+
+  @Test
   public void testApparentRank() {
     try {
       LinearSystemSolver.getSolver(new double[][] {
