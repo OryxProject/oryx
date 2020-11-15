fdfc4e829b5b9bc2708737d45fc259b0cf79cf2c
Wang <chw6@fa20-cs527-063.cs.illinois.edu>
2020-11-14 23:33:26 -0600

    Sort import position

--- framework/oryx-common/src/test/java/com/cloudera/oryx/common/text/TextUtilsTest.java
+++ framework/oryx-common/src/test/java/com/cloudera/oryx/common/text/TextUtilsTest.java
@@ -19,8 +19,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
